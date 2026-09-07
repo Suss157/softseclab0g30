7062c0dc79f796e32978a621f58261780984ce5e
Iman <imanabdusemed@gmail.com>
2026-09-07 11:33:25 +0200

    Resolve merge conflicts and update gitignore

--- src/api.py
+++ src/api.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/hello")
+<<<<<<< HEAD
+@app.route("/hello", methods=["GET"])
 def hello():
-    return jsonify({"message": "Hello World!"})
+    return jsonify({"message": "Hello, World!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+>>>>>>> 9784433c940b4ea819eca7b41475a74f7d6375c1
